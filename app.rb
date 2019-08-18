50221bc2e1838cff090527a2ac90436735151b17
Lauren McCoy <lmccoy8911@gmail.com>
2019-08-18 20:01:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    @new = Puppy.new(params[:name], params[:breed], params[:age])
+    @new = params[:name], params[:breed], params[:age]
     erb :index
   end 
   
