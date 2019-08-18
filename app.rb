bce5c56b588bd5bf70e42543d67d0ad20b460bd3
Lauren McCoy <lmccoy8911@gmail.com>
2019-08-18 20:16:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,8 @@ class App < Sinatra::Base
   
   post '/puppy' do
     @name = params['name']
-    
+    @age = params['age']
+    @breed = params['breed']
     @user_input = Puppy.new(params[:name], params[:breed],params[:age])
     erb :display_puppy
   end 
